ca470a6d80dab4c56eb6df84a209f81a9b50eb9a
Julian Allegretti <julianallegretti@MacBook-Pro-de-Julian.local>
2022-07-09 13:05:20 -0500

    validate straight in another function

--- main.go
+++ main.go
@@ -31,23 +31,7 @@ func IsStraight(cards []int) bool {
 		current := cards[i]
 		next := cards[i+1]
 
-		if current > 14 || next > 14 {
-			return false
-		}
-
-		if current < 2 || next < 2 {
-			return false
-		}
-
-		if current == 14 {
-			current = 1
-		}
-
-		if next == 14 {
-			next = 1
-		}
-
-		if current+1 != next {
+		if !validateStraight(current, next) {
 			return false
 		}
 
@@ -71,4 +55,28 @@ func sortCards(cards []int) []int{
 	})
 
 	return cards
+}
+
+func validateStraight(currentCard int, nextCard int) bool{
+	if currentCard > 14 || nextCard > 14 {
+		return false
+	}
+
+	if currentCard < 2 || nextCard < 2 {
+		return false
+	}
+
+	if currentCard == 14 {
+		currentCard = 1
+	}
+
+	if nextCard == 14 {
+		nextCard = 1
+	}
+
+	if (currentCard + 1) != nextCard {
+		return false
+	}
+
+	return true
 }
