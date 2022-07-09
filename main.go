88b7db5c7db798b54957db7dc41b4446fb5ef6ad
Julian Allegretti <julianallegretti@MacBook-Pro-de-Julian.local>
2022-07-09 14:24:11 -0500

    comentarios en metodos

--- main.go
+++ main.go
@@ -5,16 +5,16 @@ import (
 	"sort"
 )
 
-
+/***
+	Main method to identify if cards are straight
+***/
 func IsStraight(cards []int) bool {
-	if len(cards) < 4 {
+	done := initialValidate(cards)
+	if !done {
 		return false
 	}
 
-	if len(cards) > 7 {
-		return false
-	}
-
+	var straight []int
 	cards = sortCards(cards)
 
 	i := 0
@@ -26,16 +26,54 @@ func IsStraight(cards []int) bool {
 		current := cards[i]
 		next := cards[i+1]
 
-		if !validateStraight(current, next) {
-			return false
+		if len(straight) == 5 {
+			break
 		}
 
+		if validateStraight(current, next) {
+			straight = append(straight, current)
+
+			if len(straight) == 5 {
+				break
+			}
+
+			if i == (len(cards) - 2) {
+				straight = append(straight, next)
+			}
+
+			i++
+			continue
+		}
+
+		straight = []int{}
 		i++
+	}
+
+	if len(straight) == 5 {
+		return true
+	}
+
+	return false
+}
+
+/***
+	Validates the initial conditions to be a straight
+***/
+func initialValidate(cards []int) bool {
+	if len(cards) < 4 {
+		return false
+	}
+
+	if len(cards) > 7 {
+		return false
 	}
 
 	return true
 }
 
+/***
+	Sort the cards ascending
+***/
 func sortCards(cards []int) []int{
 	sort.Slice(cards, func(i, j int) bool {
 		if cards[j] == 14 {
@@ -52,6 +90,9 @@ func sortCards(cards []int) []int{
 	return cards
 }
 
+/***
+	Valid that the next card is the one indicated
+***/
 func validateStraight(currentCard int, nextCard int) bool{
 	if currentCard > 14 || nextCard > 14 {
 		return false
@@ -78,7 +119,7 @@ func validateStraight(currentCard int, nextCard int) bool{
 
 
 func main()  {
-	if IsStraight([]int{14, 3, 2, 6, 4 ,5}) {
+	if IsStraight([]int{2,7,8,5,10,9,11}) {
 		fmt.Println("IsStraight")
 		return
 	}
